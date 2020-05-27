5b72b26f7facc0335bf27021aac51fb70ea6aab6
Choi Jung Hyun <cjhnim@gmail.com>
2020-05-27 14:40:11 +0900

    initial

---
+++ tictactoe1/src/test/java/BoardTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoardTest {
+    @Test
+    public void updateBoardWithUsersMove() {
+        assertEquals(1,1);
+    }
+}
