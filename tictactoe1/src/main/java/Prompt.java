56e86d423b3ed7f8d29b10a7b91cd6160486fa9a
Choi Jung Hyun <cjhnim@gmail.com>
2020-05-28 11:21:26 +0900

    promptsForNextMove

--- tictactoe1/src/main/java/Prompt.java
+++ tictactoe1/src/main/java/Prompt.java
@@ -1,2 +1,15 @@
+import java.io.IOException;
+import java.io.Writer;
+
 public class Prompt {
+    private Writer writer;
+
+    public Prompt(Writer writer) {
+
+        this.writer = writer;
+    }
+
+    public void askForNextMove() throws IOException {
+        writer.write("Please enter move:");
+    }
 }
--- tictactoe1/src/test/java/PromptTest.java
+++ tictactoe1/src/test/java/PromptTest.java
@@ -1,8 +1,23 @@
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class PromptTest {
     @Test
     public void MakeNewInstance() {
-        Prompt prompt = new Prompt();
+        Prompt prompt = new Prompt(null);
+    }
+
+    @Test
+    public void promptsForNextMove() throws IOException {
+        Writer writer = new StringWriter();
+        Prompt prompt = new Prompt(writer);
+        prompt.askForNextMove();
+        assertThat(writer.toString(), is("Please enter move:"));
     }
 }
